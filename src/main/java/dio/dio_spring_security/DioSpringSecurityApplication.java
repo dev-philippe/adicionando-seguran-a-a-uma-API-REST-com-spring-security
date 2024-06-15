301f537c7bd8952827ae7f87ded567d483e54188
unknown <philippediniz@outlook.com>
2024-06-15 18:15:03 -0300

    Autenticação simples

--- src/main/java/dio/dio_spring_security/DioSpringSecurityApplication.java
+++ src/main/java/dio/dio_spring_security/DioSpringSecurityApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DioSpringSecurityApplication {
+public class DioSpringSecurityApplicatio
 
 	public static void main(String[] args) {
 		SpringApplication.run(DioSpringSecurityApplication.class, args);
+
+		System.out.println("Helloo Word");git status
 	}
+
+
 
 }
